f6398f33d7f0b2def932786f216bdbf2f85309df
karim mdmirajul <karim.mdmirajul@unikie.com>
2023-11-07 09:55:15 +0200

    create full-func vm
    Signed-off-by: karim mdmirajul <karim.mdmirajul@unikie.com>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,9 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.ghaf_infra_tf_dev.name
 }
 
 output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
+  value = var.resource_group_location
 }
