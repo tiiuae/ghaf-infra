be94be2ff23d78f03cad4d59b30d00f2b1b9ac4e
Brian McGee <brian@bmcgee.ie>
2023-11-09 13:08:06 +0000

    fix: Copyright headers
    Adds origin in `default.nix` and `shell.nix` as they
    were copied from samples.
    
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
 # SPDX-FileCopyrightText: 2020-2023 Eelco Dolstra and the flake-compat contributors
 #
 # SPDX-License-Identifier: MIT
+# This file originates from:
+# https://github.com/nix-community/flake-compat
 # This file provides backward compatibility to nix < 2.4 clients
 {system ? builtins.currentSystem}: let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
