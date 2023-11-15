30ba7b43b67a37524579c6accaebec6aafb882db
Joonas Rautiola <joonas.rautiola@unikie.com>
2023-11-15 20:53:45 +0200

    Add monitoring server configuration
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

---
+++ hosts/monitoring/disk-config.nix
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+#
+# SPDX-License-Identifier: Apache-2.0
+# BIOS compatible gpt partition
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        device = "/dev/vda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
