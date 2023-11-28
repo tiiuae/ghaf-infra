f4cc7190e9387deeaacd9163e4e9f261b7f44f51
Henri Rosten <henri.rosten@unikie.com>
2023-11-28 09:55:09 +0200

    Add ghaf-dev cachix cache
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- hosts/common.nix
+++ hosts/common.nix
@@ -25,10 +25,12 @@ in {
       experimental-features = "nix-command flakes";
       # Subsituters
       trusted-public-keys = [
+        "ghaf-dev.cachix.org-1:S3M8x3no8LFQPBfHw1jl6nmP8A7cVWKntoMKN3IsEQY="
         "cache.vedenemo.dev:8NhplARANhClUSWJyLVk4WMyy1Wb4rhmWW2u8AejH9E="
         "cache.ssrcdevops.tii.ae:oOrzj9iCppf+me5/3sN/BxEkp5SaFkHfKTPPZ97xXQk="
       ];
       substituters = [
+        "https://ghaf-dev.cachix.org?priority=20"
         "https://cache.vedenemo.dev"
         "https://cache.ssrcdevops.tii.ae"
       ];
