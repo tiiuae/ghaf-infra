cac26576e42eb819572b6a3fd04c2c407a531dfe
Marko Kaapu <marko.kaapu@unikie.com>
2023-11-29 15:10:02 +0200

    Remove the networking.useDHCP configuration field (using default value)
    Signed-off-by: Marko Kaapu <marko.kaapu@unikie.com>

--- hosts/ficolobuild/default.nix
+++ hosts/ficolobuild/default.nix
@@ -28,12 +28,6 @@
   boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "megaraid_sas" "nvme" "usbhid" "sd_mod"];
   boot.kernelModules = ["kvm-intel"];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
