25eb9c5e45662b2f252bf61920fb37b5948a0fd3
Florian Klink <flokli@flokli.de>
2023-12-05 13:51:21 +0200

    tf-modules: init azurerm-linux-vm
    This groups some common together some resources to create a VM. We might
    introduce more flexibility at a later point.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

---
+++ tf-modules/azurerm-linux-vm/README.md
@@ -0,0 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+# azurerm-linux-vm
+
+Terraform module spinning up a Azure VM.
---
+++ tf-modules/azurerm-linux-vm/variables.tf
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+#
+# SPDX-License-Identifier: Apache-2.0
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "virtual_machine_name" {
+  type = string
+}
+
+variable "virtual_machine_size" {
+  type = string
+}
+
+variable "virtual_machine_source_image" {
+  type = string
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The subnet ID to attach to the VM and allocate an IP from"
+}
+
