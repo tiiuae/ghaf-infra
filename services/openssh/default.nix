6fdeb5643427e179c18787f66b44827e9ac8d618
Florian Klink <flokli@flokli.de>
2023-12-06 16:00:34 +0200

    services/openssh: set with priorities
    azure-common.nix already sets
    services.openssh.settings.{PermitRootLogin,ClientAliveInterval},
    so we need to decide what wins.
    
    To keep the intended behaviour, we want to mkForce PermitRootLogin to
    "no" (azure-common.nix sets "prohibit-password"), and set the
    ClientAliveInterval with mkDefault - bumping that timeout probably makes
    sense for azure, and we don't want the setting in this file to take
    priority.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

--- services/openssh/default.nix
+++ services/openssh/default.nix
@@ -3,13 +3,15 @@
 # SPDX-License-Identifier: Apache-2.0
 {
   pkgs,
+  lib,
+  ...
 }: {
   services.openssh = {
     enable = true;
-    settings.PermitRootLogin = "no";
+    settings.PermitRootLogin = lib.mkForce "no";
     settings.KbdInteractiveAuthentication = false;
     settings.PasswordAuthentication = false;
-    settings.ClientAliveInterval = 60;
+    settings.ClientAliveInterval = lib.mkDefault 60;
     hostKeys = [
       {
         path = "/etc/ssh/ssh_host_ed25519_key";
