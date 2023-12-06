214de329a7c17fd6b885396e89151685d67fd0c7
Florian Klink <flokli@flokli.de>
2023-12-06 20:42:25 +0200

    hosts/azure-common-2: init
    Move the azure-specific config snipped into its own file, so we can
    import it from multiple configuration.nix.
    
    azure-common.nix is already used for the existing machine
    configurations, and as we don't want to break these, it's using this
    transient name.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

--- hosts/jenkins-controller/configuration.nix
+++ hosts/jenkins-controller/configuration.nix
@@ -4,28 +4,14 @@
 {
   self,
   lib,
-  modulesPath,
   ...
 }: {
   imports = [
-    "${modulesPath}/virtualisation/azure-config.nix"
+    ../azure-common-2.nix
     self.nixosModules.service-openssh
   ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-
-  # enable cloud-init, so instance metadata is set accordingly and we can use
-  # cloud-config for ssh key management.
-  services.cloud-init.enable = true;
-
-  # Use systemd-networkd for network configuration
-  services.cloud-init.network.enable = true;
-  networking.useDHCP = false;
-  networking.useNetworkd = true;
-  # FUTUREWORK: Ideally, we'd keep systemd-resolved disabled too,
-  # but the way nixpkgs configures cloud-init prevents it from picking up DNS
-  # settings from elsewhere.
-  # services.resolved.enable = false;
 
   system.stateVersion = "23.05";
 }
