57970855ac7a789c57872daf5b1c6b01b608bd74
Florian Klink <flokli@flokli.de>
2023-12-06 16:23:33 +0200

    terraform/jenkins: init
    This builds the jenkins-master Nix image, turns it into a bootable Azure
    image, and then boots an instance with the image.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

---
+++ terraform/jenkins/jenkins_controller.tf
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Build the Jenkins controller image
+module "jenkins_controller_image" {
+  source = "../../tf-modules/azurerm-nix-vm-image"
+
+  nix_attrpath   = "outputs.nixosConfigurations.jenkins-controller.config.system.build.azureImage"
+  nix_entrypoint = "${path.module}/../.."
+
+
+  name                = "jenkins-controller"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+
+  storage_account_name   = azurerm_storage_account.vm_images.name
+  storage_container_name = azurerm_storage_container.vm_images.name
+}
+
+# Create a machine using this image
+module "jenkins_controller_vm" {
+  source = "../../tf-modules/azurerm-linux-vm"
+
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+
+  virtual_machine_name         = "ghaf-jenkins-controller"
+  virtual_machine_size         = "Standard_D1_v2"
+  virtual_machine_source_image = module.jenkins_controller_image.image_id
+
+  virtual_machine_custom_data = join("\n", ["#cloud-config", yamlencode({
+    users = [
+      for user in toset(["bmg", "flokli", "hrosten"]) : {
+        name                = user
+        sudo                = "ALL=(ALL) NOPASSWD:ALL"
+        ssh_authorized_keys = local.ssh_keys[user]
+      }
+    ]
+  })])
+
+  subnet_id = azurerm_subnet.jenkins.id
+}
