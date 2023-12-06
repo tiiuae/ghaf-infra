b879d35ce39b93c823f2402a148422981f5fab05
Florian Klink <flokli@flokli.de>
2023-12-06 20:39:10 +0200

    hosts: add binary-cache config
    This exposes a read-only HTTP webserver for the contents in the storage
    container.
    `rclone serve http` takes care of exposing the storage container over
    HTTP.
    
    We disallow listing (by only allowing access to certain paths), and
    expose it over HTTP(S) with auto-ssl via caddy.
    
    This will work with whatever domain we route to it, so it's not part of
    the configuration.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

---
+++ hosts/binary-cache/configuration.nix
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+#
+# SPDX-License-Identifier: Apache-2.0
+{
+  self,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ../azure-common-2.nix
+    self.nixosModules.service-openssh
+  ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # Run a read-only HTTP webserver proxying to the "binary-cache-v1" storage
+  # container at a unix socket.
+  # This relies on IAM to grant access to the storage container.
+  systemd.services.rclone-http = {
+    after = ["network.target"];
+    requires = ["network.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "notify";
+      Restart = "always";
+      RestartSec = 2;
+      DynamicUser = true;
+      RuntimeDirectory = "rclone-http";
+      # FUTUREWORK: set AZURE_STORAGE_ACCOUNT_NAME and storage container name through EnvironmentFile
+      ExecStart =
+        "${pkgs.rclone}/bin/rclone "
+        + "serve http "
+        + "--azureblob-env-auth "
+        + "--azureblob-account ghafbinarycache "
+        + "--read-only "
+        + "--addr unix://%t/rclone-http/socket "
+        + ":azureblob:binary-cache-v1";
+      # On successful startup, grant caddy write permissions to the socket.
+      ExecStartPost = "${pkgs.acl.bin}/bin/setfacl -m u:caddy:rw %t/rclone-http/socket";
+    };
+  };
+
+  # Expose the rclone-http unix socket over a HTTPS, limiting to certain
+  # keys only, disallowing listing too.
+  services.caddy = {
+    enable = true;
+    configFile = pkgs.writeTextDir "Caddyfile" ''
+      # Disable the admin API, we don't want to reconfigure Caddy at runtime.
+      {
+        admin off
+      }
+
+      # Proxy a subset of requests to rclone.
+      * {
+        handle /nix-cache-info {
+          reverse_proxy unix///run/rclone-http/socket
+        }
+        handle /*.narinfo {
+          reverse_proxy unix///run/rclone-http/socket
+        }
+        handle /nar/*.nar {
+          reverse_proxy unix///run/rclone-http/socket
+        }
+        handle /nar/*.nar.* {
+          reverse_proxy unix///run/rclone-http/socket
+        }
+      }
+    '';
+  };
+
+  # Expose the HTTP and HTTPS port.
+  networking.firewall.allowedTCPPorts = [80 443];
+
+  system.stateVersion = "23.05";
+}
