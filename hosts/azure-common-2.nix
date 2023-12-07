b7887b84776ca4b0a0c1b1e19bc72f24a6609baf
Florian Klink <flokli@flokli.de>
2023-12-07 17:44:27 +0200

    azure-common: support timeout in disk_setup
    We need to use cloud-init to format and mount data volumes in azure, we
    can't use systemd for it.
    
    Due to
    https://github.com/hashicorp/terraform-provider-azurerm/issues/6117,
    disks in Azure gets attached late at boot, so any dev-disk-by-….device
    units created via systemd-fstab-generator might not exist yet at the
    time the graph for multi-user.target is created, causing systemd to fail
    starting downstream services due to a missing dependency.
    
    Once the volume is attached, the .device unit pops up via udev, and then
    a manual restart of services depending on data disks would work, but
    it's messy.
    
    Letting cloud-init take care of data disk mounting (and formatting) is
    the right choice, that way systemd doesn't need to do any dependency
    tracking of it.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

--- hosts/azure-common-2.nix
+++ hosts/azure-common-2.nix
@@ -4,9 +4,30 @@
 #
 # Profile to import for Azure VMs. Imports azure-common.nix from nixpkgs,
 # and configures cloud-init.
-{modulesPath, ...}: {
+{
+  modulesPath,
+  pkgs,
+  ...
+}: {
   imports = [
     "${modulesPath}/virtualisation/azure-config.nix"
+  ];
+
+  nixpkgs.overlays = [
+    (_self: super: {
+      cloud-init = super.cloud-init.overrideAttrs (old: {
+        patches =
+          old.patches
+          or []
+          ++ [
+            # Add support for timeout in disk_setup: https://github.com/canonical/cloud-init/pull/4673
+            (pkgs.fetchpatch {
+              url = "https://github.com/canonical/cloud-init/pull/4673/commits/9b2e3dc907dc06d0a2abdaae6f0b1f0612c5c5dc.patch";
+              hash = "sha256-KAd+4YT+dgzIoEq5qZj6y4peclIb3rvnuY6QIQObAiY=";
+            })
+          ];
+      });
+    })
   ];
 
   # enable cloud-init, so instance metadata is set accordingly and we can use
