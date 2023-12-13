51860e1ad3b119d2301eb5825e2abf2d48456009
Florian Klink <flokli@flokli.de>
2023-12-13 15:13:17 +0200

    hosts: use x-systemd.device-timeout=5min option
    Apparently https://github.com/canonical/cloud-init/pull/4673 and more
    hacks are not needed, we can simply ramp up the timeout that systemd is
    willing to wait for the .device unit to appear.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

--- hosts/azure-common-2.nix
+++ hosts/azure-common-2.nix
@@ -4,30 +4,9 @@
 #
 # Profile to import for Azure VMs. Imports azure-common.nix from nixpkgs,
 # and configures cloud-init.
-{
-  modulesPath,
-  pkgs,
-  ...
-}: {
+{modulesPath, ...}: {
   imports = [
     "${modulesPath}/virtualisation/azure-config.nix"
-  ];
-
-  nixpkgs.overlays = [
-    (_self: super: {
-      cloud-init = super.cloud-init.overrideAttrs (old: {
-        patches =
-          old.patches
-          or []
-          ++ [
-            # Add support for timeout in disk_setup: https://github.com/canonical/cloud-init/pull/4673
-            (pkgs.fetchpatch {
-              url = "https://github.com/canonical/cloud-init/pull/4673/commits/9b2e3dc907dc06d0a2abdaae6f0b1f0612c5c5dc.patch";
-              hash = "sha256-KAd+4YT+dgzIoEq5qZj6y4peclIb3rvnuY6QIQObAiY=";
-            })
-          ];
-      });
-    })
   ];
 
   # enable cloud-init, so instance metadata is set accordingly and we can use
@@ -42,9 +21,4 @@
   # but the way nixpkgs configures cloud-init prevents it from picking up DNS
   # settings from elsewhere.
   # services.resolved.enable = false;
-
-  # Add filesystem-related tools to cloud-inits path, so it can format data disks.
-  services.cloud-init.btrfs.enable = true;
-  services.cloud-init.ext4.enable = true;
-  services.cloud-init.xfs.enable = true;
 }
