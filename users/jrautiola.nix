ee418094b2bed7e8c3ee1972e52b5c9c284dd5fb
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-01-10 19:15:39 +0200

    Add new ssh keys for user jrautiola
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- users/jrautiola.nix
+++ users/jrautiola.nix
@@ -7,7 +7,8 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII6EoeiMBiiwfGJfQYyuBKg8rDpswX0qh194DUQqUotL joonas@buutti"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPjn9ZyVUkSuhcfpAtjrEQn2g1MhQdKc3vLLRqCz0tWk joonas@unikie"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlFqSQFoSSuAS1IjmWBFXie329I5Aqf71QhVOnLTBG+ joonas@x1"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3h/Aj66ndKFtqpQ8H53tE9KbbO0obThC0qbQQKFQRr joonas@zeus"
       ];
       extraGroups = ["wheel" "networkmanager"];
     };
