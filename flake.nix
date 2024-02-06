831bd4e555854e469edc3ec977a0f1c5dc0c4207
Henri Rosten <henri.rosten@unikie.com>
2024-02-06 13:07:46 +0200

    flake: move back to nixos-23.11
    This reverts 3ba044e45ad0a0e7667de89d8cab87fb5dc8215b moving the nixpkgs
    revision back to b0b2c5445c64191fd8d0b31f2b1a34e45a64547d from 23.11
    which is the same nixpkgs version as what was used in main-branch
    already.
    
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     # Allows us to structure the flake with the NixOS module system
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-root.url = "github:srid/flake-root";
