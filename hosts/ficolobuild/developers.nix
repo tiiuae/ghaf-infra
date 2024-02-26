975cfcf42d7c53198198d39fd9d0c6349bd3d4be
Marko Kaapu <marko.kaapu@unikie.com>
2024-02-26 16:31:25 +0200

    x86 builder: Add a new SSH key to bgm
    Signed-off-by: Marko Kaapu <marko.kaapu@unikie.com>

--- hosts/ficolobuild/developers.nix
+++ hosts/ficolobuild/developers.nix
@@ -17,6 +17,7 @@
       keys = [
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEJ9ewKwo5FLj6zE30KnTn8+nw7aKdei9SeTwaAeRdJDAAAABHNzaDo="
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIA/pwHnzGNM+ZU4lANGROTRe2ZHbes7cnZn72Oeun/MCAAAABHNzaDo="
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILu6O3swRVWAjP7J8iYGT6st7NAa+o/XaemokmtKdpGa builder key"
       ];
     }
     {
