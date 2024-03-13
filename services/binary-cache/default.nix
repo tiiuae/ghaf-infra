d879896d90f82bed93a8ba1152f741d47c73d2c5
Tero Tervala <tero.tervala@unikie.com>
2024-03-13 15:55:41 +0200

    Replace nix-serve with harmonia on binarycache
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- services/binary-cache/default.nix
+++ services/binary-cache/default.nix
@@ -1,16 +1,10 @@
 # SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{
-  config,
-  inputs,
-  pkgs,
-  ...
-}: {
+{config, ...}: {
   services = {
-    nix-serve = {
+    harmonia = {
       enable = true;
-      package = inputs.nix-serve-ng.packages.${pkgs.system}.default;
-      secretKeyFile = config.sops.secrets.cache-sig-key.path;
+      signKeyPath = config.sops.secrets.cache-sig-key.path;
     };
   };
   networking.firewall.allowedTCPPorts = [5000];
