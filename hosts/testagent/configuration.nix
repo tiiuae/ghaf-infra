e893d3a2064637b9bdd93a0a74489eb9b8c503a3
Tero Tervala <tero.tervala@unikie.com>
2024-04-02 10:33:47 +0300

    Basic setup of the testagent
    Also added copyrights to testagent .nix files
    
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- hosts/testagent/configuration.nix
+++ hosts/testagent/configuration.nix
@@ -1,16 +1,30 @@
+# SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
+  self,
+  inputs,
+  ...
+}: {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  nix.settings.experimental-features = "nix-command flakes";  
+    [
+      # Include the results of the hardware scan.
+      ./testagent-hardware-configuration.nix
+    ]
+    ++ (with inputs; [
+      sops-nix.nixosModules.sops
+      disko.nixosModules.disko
+    ])
+    ++ (with self.nixosModules; [
+      common
+      service-openssh
+      user-tervis
+      user-vjuntunen
+      user-flokli
+    ]);
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -25,9 +39,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Helsinki";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -69,7 +80,7 @@
   # Enable the GNOME Desktop Environment.
   #services.xserver.displayManager.gdm.enable = true;
   #services.xserver.desktopManager.gnome.enable = true;
-  
+
   # Disable gdm suspend
   #services.xserver.displayManager.gdm.autoSuspend = false;
 
@@ -101,82 +112,4 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users = {
-    tervis = {
-      isNormalUser = true;
-      description = "Tero Tervala";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJau0tg0qHhqFVarjNOJLi+ekSZNNqxal4iRD/pwM5W tervis@tervis-thinkpad"
-      ];
-      extraGroups = [ "networkmanager" "wheel" ];
-    };
-
-    flokli = {
-      isNormalUser = true;
-      description = "Florian Klink";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPTVTXOutUZZjXLB0lUSgeKcSY/8mxKkC0ingGK1whD2 flokli"
-      ];
-      extraGroups = [ "networkmanager" "wheel" ];
-    };
-
-    vjuntunen = {
-      isNormalUser = true;
-      description = "Ville-Pekka Juntunen";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJvhDUiX05DpsnQ1T8Fmoj2qVrInF8NeDSm8WiDeIxR"
-      ];
-      extraGroups = [ "networkmanager" "wheel" ];
-    };
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    dnsutils
-    wget
-    curl
-    gitFull
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-  
-  # Allow password-less sudo for wheel users
-  security.sudo.enable = true;
-  security.sudo.wheelNeedsPassword = false;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
 }
