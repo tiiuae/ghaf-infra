c6e5830b83cc119997f9aeea872b5c877e2628dc
Florian Klink <flokli@flokli.de>
2024-04-10 17:14:27 +0300

    testagent: use more stable block device paths
    Using the UUIDs will break as soon as we recreate the filesystem.
    
    Using nvme0n1[p{1,2}] is actually more stable here.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

--- hosts/testagent/configuration.nix
+++ hosts/testagent/configuration.nix
@@ -12,7 +12,7 @@
   imports =
     [
       # Include the results of the hardware scan.
-      ./testagent-hardware-configuration.nix
+      ./hardware-configuration.nix
     ]
     ++ (with inputs; [
       sops-nix.nixosModules.sops
