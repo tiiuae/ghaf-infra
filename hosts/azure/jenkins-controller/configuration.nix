505edbbd3a3275897a3404a9141e57691865aa52
Marko Kaapu <marko.kaapu@unikie.com>
2024-04-11 13:38:49 +0000

    Add Jenkins pipeline stage view
    Installs Pipeline Stage View Plugin.
    
    Signed-off-by: Marko Kaapu <marko.kaapu@unikie.com>

--- hosts/azure/jenkins-controller/configuration.nix
+++ hosts/azure/jenkins-controller/configuration.nix
@@ -179,7 +179,7 @@ in {
       jenkins-auth = "-auth admin:\"$(cat /var/lib/jenkins/secrets/initialAdminPassword)\"";
     in ''
       # Install plugins
-      jenkins-cli ${jenkins-auth} install-plugin "workflow-aggregator" "github" "timestamper" -deploy
+      jenkins-cli ${jenkins-auth} install-plugin "workflow-aggregator" "github" "timestamper" "pipeline-stage-view" -deploy
 
       # Jenkins groovy config
       jenkins-cli ${jenkins-auth} groovy = < ${jenkins-groovy}
