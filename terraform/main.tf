0ba691e04f010b15bb44d2038998ed439cbac9b1
Fayad Fami <fayadx@ssrc.tii.ae>
2024-05-13 11:25:25 +0400

    Update with vnet peering configurations - Add peering arguments

--- terraform/main.tf
+++ terraform/main.tf
@@ -207,6 +207,11 @@ resource "azurerm_virtual_network_peering" "source-vnet" {
   resource_group_name       = azurerm_resource_group.source-rg.name
   virtual_network_name      = azurerm_virtual_network.source-vnet.name
   remote_virtual_network_id = azurerm_virtual_network.destination-vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic = true
+  allow_gateway_transit = true
+  use_remote_gateways = true
+
 }
 
 resource "azurerm_virtual_network_peering" "destination-vnet" {
@@ -214,6 +219,10 @@ resource "azurerm_virtual_network_peering" "destination-vnet" {
   resource_group_name       = azurerm_resource_group.destination-rg.name
   virtual_network_name      = azurerm_virtual_network.destination-vnet.name
   remote_virtual_network_id = azurerm_virtual_network.source-vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic = true
+  allow_gateway_transit = true
+  use_remote_gateways = true
 }
 
 ################################################################################
