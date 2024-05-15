ffe534575f301aa1f9d43fe4cd86198ab0fb16a9
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-05-15 13:00:40 +0300

    Temporarily remove hetzarm target from tasks
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- tasks.py
+++ tasks.py
@@ -95,10 +95,6 @@ TARGETS = OrderedDict(
             hostname="172.18.16.60",
             nixosconfig="testagent",
         ),
-        "hetzarm": TargetHost(
-            hostname="65.21.20.242",
-            nixosconfig="hetzarm",
-        ),
     }
 )
 
