03295a34b0d04f0d5fac9195254be1f903d99f6a
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-05-22 11:29:04 +0300

    Add binfmt aarch64 emulation
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

---
+++ hosts/builders/cross-compilation.nix
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  # list of systems to emulate using binfmt
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
+
+  # extra nix build platforms to enable
+  nix.settings.extra-platforms = ["i686-linux"];
+}
