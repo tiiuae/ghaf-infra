2a01c66a24c97b9d614bcda43b8be50637ff1f6c
Henri Rosten <henri.rosten@unikie.com>
2024-05-30 14:02:51 +0300

    Add user ktu to jenkins-controller
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- terraform/jenkins-controller.tf
+++ terraform/jenkins-controller.tf
@@ -32,7 +32,7 @@ module "jenkins_controller_vm" {
 
   virtual_machine_custom_data = join("\n", ["#cloud-config", yamlencode({
     users = [
-      for user in toset(["bmg", "flokli", "hrosten", "jrautiola", "mkaapu", "mika", "karim", "tervis", "cazfi", "vjuntunen"]) : {
+      for user in toset(["bmg", "flokli", "hrosten", "jrautiola", "mkaapu", "mika", "karim", "tervis", "cazfi", "vjuntunen", "ktu"]) : {
         name                = user
         sudo                = "ALL=(ALL) NOPASSWD:ALL"
         ssh_authorized_keys = local.ssh_keys[user]
