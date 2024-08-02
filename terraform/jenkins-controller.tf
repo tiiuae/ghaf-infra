2fa485f830f810fcf97b56123c9e004dacbb488d
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2024-08-02 11:23:50 +0300

    Add Alex pubkey
    Signed-off-by: Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>

--- terraform/jenkins-controller.tf
+++ terraform/jenkins-controller.tf
@@ -32,7 +32,7 @@ module "jenkins_controller_vm" {
 
   virtual_machine_custom_data = join("\n", ["#cloud-config", yamlencode({
     users = [
-      for user in toset(["bmg", "flokli", "hrosten", "jrautiola", "mkaapu", "mika", "karim", "tervis", "cazfi", "vjuntunen", "ktu"]) : {
+      for user in toset(["bmg", "flokli", "hrosten", "jrautiola", "mkaapu", "mika", "karim", "tervis", "cazfi", "vjuntunen", "ktu", "alextserepov"]) : {
         name                = user
         sudo                = "ALL=(ALL) NOPASSWD:ALL"
         ssh_authorized_keys = local.ssh_keys[user]
