97767f81d45f8ba1f2e36e7498ea1331ae750c37
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-08-15 13:55:35 +0300

    Implement Github OAuth for ghaf-log grafana login
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- hosts/ghaf-log/configuration.nix
+++ hosts/ghaf-log/configuration.nix
@@ -35,8 +35,13 @@
       user-vunnyso
     ]);
 
-  # basic auth credentials generated with htpasswd
-  sops.secrets.loki_basic_auth.owner = "nginx";
+  sops.secrets = {
+    # basic auth credentials generated with htpasswd
+    loki_basic_auth.owner = "nginx";
+    # github oauth app credentials
+    github_client_id.owner = "grafana";
+    github_client_secret.owner = "grafana";
+  };
 
   nixpkgs.hostPlatform = "x86_64-linux";
   hardware.enableRedistributableFirmware = true;
@@ -76,6 +81,11 @@
         http_addr = "127.0.0.1";
         domain = "ghaflogs.vedenemo.dev";
         enforce_domain = true;
+
+        # the default root_url is unaware of our nginx reverse proxy,
+        # and tries using http with port 3000 as the redirect url for auth.
+        # https://github.com/grafana/grafana/issues/11817#issuecomment-387131608
+        root_url = "https://%(domain)s/";
       };
 
       # disable telemetry
@@ -87,9 +97,32 @@
       # https://grafana.com/docs/grafana/latest/setup-grafana/configure-security/configure-security-hardening
       security = {
         cookie_secure = true;
-        cookie_samesite = "strict";
+        # we cannot use 'strict' here or github oauth cannot set the login cookie
+        cookie_samesite = "lax";
         login_cookie_name = "__Host-grafana_session";
         strict_transport_security = true;
+      };
+
+      # github OIDC
+      "auth.github" = {
+        enabled = true;
+        client_id = "$__file{${config.sops.secrets.github_client_id.path}}";
+        client_secret = "$__file{${config.sops.secrets.github_client_secret.path}}";
+
+        # only these orgs and teams are allowed login
+        # team IDs can be found with github api:
+        # $ curl -H "Authorization: $PAT" "https://api.github.com/orgs/tiiuae/teams?per_page=100" | jq '.[] | {name, id}'
+        allowed_organizations = [ "tiiuae" ];
+        team_ids = lib.strings.concatStringsSep "," [
+          "7362549" # devenv-fi
+          "4067903" # phone
+        ];
+
+        # map github teams to grafana roles
+        role_attribute_path = lib.strings.concatStringsSep " || " [
+          "contains(groups[*], '@tiiuae/devenv-fi') && 'GrafanaAdmin'"
+          "contains(groups[*], '@tiiuae/phone') && 'Editor'"
+        ];
       };
     };
 
