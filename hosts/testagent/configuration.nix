1293f2b483e1d255970091db91d4cb6ca6f35dd1
Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>
2024-08-16 15:06:32 +0300

    HW test configuration file fix
    Fix for lenovo-x1 testagent configuration.
    
    Signed-off-by: Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>

--- hosts/testagent/configuration.nix
+++ hosts/testagent/configuration.nix
@@ -63,6 +63,11 @@ in
     pkgs.usbsdmux
   ];
 
+  # udev rules for test devices serial connections
+  services.udev.extraRules = ''
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="FTD1BQQS", SYMLINK+="ttyORINNX1", MODE="0666", GROUP="dialout"
+  '';
+
   environment.systemPackages = [
     inputs.robot-framework.packages.${pkgs.system}.ghaf-robot
     brainstem
@@ -122,6 +127,7 @@ in
       pkgs.netcat
       pkgs.python3
       pkgs.wget
+      pkgs.usbsdmux
       brainstem
       inputs.robot-framework.packages.${pkgs.system}.ghaf-robot
     ];
@@ -145,7 +151,7 @@ in
         serial_port = "NONE";
         device_ip_address = "172.18.16.50";
         socket_ip_address = "172.18.16.30";
-        plug_type = "TAPOP100";
+        plug_type = "TAPOP100v2";
         location = "testagent";
         usbhub_serial = "F0A0D6CF";
         threads = 8;
@@ -159,7 +165,7 @@ in
         usbhub_serial = "92D8AEB7";
         threads = 8;
       };
-      LenovoX1-1 = {
+      LenovoX1-2 = {
         serial_port = "NONE";
         device_ip_address = "172.18.16.66";
         socket_ip_address = "NONE";
