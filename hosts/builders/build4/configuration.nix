6d1773f3c05d8b30066b4ac7d1bb53aae5609d97
Henri Rosten <henri.rosten@unikie.com>
2024-10-25 13:50:12 +0300

    Cleanup build4 configuration
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- hosts/builders/build4/configuration.nix
+++ hosts/builders/build4/configuration.nix
@@ -6,37 +6,14 @@
     [
       ../ficolo.nix
       ../cross-compilation.nix
-      ../yubikey.nix
       ../builders-common.nix
     ]
     ++ (with self.nixosModules; [
       user-themisto
-      service-nginx
     ]);
 
   # build4 specific configuration
 
   networking.hostName = "build4";
 
-  users.users.yubimaster.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2BcpFzSXOuK9AzN+J1HBVnuVV8D3wgdEwPuILNy2aM signer"
-  ];
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "trash@unikie.com";
-  };
-
-  services.nginx = {
-    virtualHosts = {
-      "pandia.vedenemo.dev" = {
-        enableACME = true;
-        forceSSL = true;
-        default = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:3015";
-        };
-      };
-    };
-  };
 }
