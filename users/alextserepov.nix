dee9f564bdd3a5d53f43866c9251a9850c082c83
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2024-10-31 09:42:36 +0200

    Update alextserepov.nix
    Fixing a copy-paste mistake. :) 
    
    Signed-off-by: Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>

--- users/alextserepov.nix
+++ users/alextserepov.nix
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 {
   users.users = {
-    avnik = {
+    alextserepov = {
       description = "Aleksandr Tserepov-Savolainen";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
