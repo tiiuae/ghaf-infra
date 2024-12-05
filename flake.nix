763b33768c9b420677444f053d53d0d3947725a0
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-12-05 13:13:44 +0200

    Update flake to NixOS 24.11
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- flake.nix
+++ flake.nix
@@ -1,58 +1,74 @@
 # SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  description = "NixOS configurations for Ghaf";
+  description = "NixOS configurations for Ghaf Infra";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
     # Allows us to structure the flake with the NixOS module system
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-root.url = "github:srid/flake-root";
+
     # Secrets with sops-nix
     sops-nix = {
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
+
     # Disko for disk partitioning
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # Format all the things
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # For preserving compatibility with non-Flake users
     flake-compat = {
       url = "github:nix-community/flake-compat";
       flake = false;
     };
+
     # Used for deploying remote systems
     deploy-rs = {
       url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
-    # pipeline dependencies
+
+    # Utilities used in the jenkins pipelines
     robot-framework = {
       url = "github:tiiuae/ci-test-automation";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sbomnix = {
       url = "github:tiiuae/sbomnix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        flake-compat.follows = "flake-compat";
+        flake-root.follows = "flake-root";
+        treefmt-nix.follows = "treefmt-nix";
+      };
     };
     ci-yubi = {
       url = "github:tiiuae/ci-yubi";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Installed into devshell
     nix-fast-build = {
       url = "github:Mic92/nix-fast-build";
       inputs = {
-        flake-parts.follows = "flake-parts";
         nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
         treefmt-nix.follows = "treefmt-nix";
       };
     };
