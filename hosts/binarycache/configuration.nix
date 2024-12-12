5c944626b0143359d6e324e90a10818bcbf3fe93
Marko Lindqvist <marko.lindqvist@unikie.com>
2024-12-12 09:40:58 +0200

    Revert "Cleanup: hosts/binarycache configuration"
    This reverts commit 47ab96bd42caabf6f2615d48091b3176c80f4b64.
    
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- hosts/binarycache/configuration.nix
+++ hosts/binarycache/configuration.nix
@@ -5,10 +5,12 @@
   self,
   inputs,
   lib,
+  config,
   ...
 }:
 {
   sops.defaultSopsFile = ./secrets.yaml;
+  sops.secrets.cache-sig-key.owner = "root";
 
   imports =
     [
@@ -18,8 +20,11 @@
     ]
     ++ (with self.nixosModules; [
       common
+      qemu-common
       ficolo-common
       service-openssh
+      service-binary-cache
+      service-nginx
       user-jrautiola
       user-cazfi
       user-hrosten
@@ -27,11 +32,42 @@
       user-avnik
     ]);
 
+  nix.settings = {
+    # we don't want the cache to be a substitutor for itself
+    substituters = lib.mkForce [ "https://cache.nixos.org/" ];
+  };
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  networking.hostName = "binarycache";
+
+  networking = {
+    hostName = "binarycache";
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "trash@unikie.com";
+  };
 
   services.monitoring = {
     metrics.enable = true;
     logs.enable = true;
+  };
+
+  services.nginx = {
+    recommendedZstdSettings = true;
+    virtualHosts = {
+      "cache.vedenemo.dev" = {
+        enableACME = true;
+        forceSSL = true;
+        default = true;
+        locations."/" = {
+          proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
+          extraConfig = ''
+            zstd on;
+            zstd_types application/x-nix-archive;
+          '';
+        };
+      };
+    };
   };
 }
