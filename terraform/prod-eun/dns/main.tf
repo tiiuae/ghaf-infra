b92cd15505d4745f3e68275ca015bf6e6ea105e4
Florian Klink <flokli@flokli.de>
2024-12-19 15:02:44 +0200

    terraform/prod-eun: merge variables.tf -> main.tf
    These states are fairly small, it all fits into the main.tf.
    
    Signed-off-by: Florian Klink <flokli@flokli.de>

--- terraform/prod-eun/dns/main.tf
+++ terraform/prod-eun/dns/main.tf
@@ -1,6 +1,14 @@
 # SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 
+variable "resource_group_name" {
+  type = string
+}
+
+variable "environment_name" {
+  type = string
+}
+
 locals {
   dns_suffix = "${replace(var.environment_name, "/^ghaf-infra-/", "")}.az.vedenemo.dev"
 }
--- terraform/prod-eun/rg/main.tf
+++ terraform/prod-eun/rg/main.tf
@@ -1,6 +1,14 @@
 # SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 
+variable "environment_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
 resource "azurerm_resource_group" "default" {
   name     = var.environment_name
   location = var.location
