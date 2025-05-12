2c43aa00b54d32304199ccecdf8235b720892a20
Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>
2025-05-12 08:51:09 +0300

    Add Orin AGX 64GB
    Add Oring AGX 64BG to testagents configurations
    Add udev rules for both Orin agx per agent
    Add own agent and agent service for agx64
    
    Signed-off-by: Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>

--- hosts/testagent/dev/configuration.nix
+++ hosts/testagent/dev/configuration.nix
@@ -33,7 +33,7 @@
     hardware = [
       "orin-agx"
       "orin-nx"
-      "nuc"
+      "orin-agx-64"
       "lenovo-x1"
       "dell-7330"
     ];
@@ -57,9 +57,20 @@
 
   # udev rules for test devices serial connections
   services.udev.extraRules = ''
+    # Orin nx
     SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="FTD1BQQS", SYMLINK+="ttyORINNX1", MODE="0666", GROUP="dialout"
-    SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="FTD0WF8Y", SYMLINK+="ttyNUC1", MODE="0666", GROUP="dialout"
-    SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea71", ATTRS{serial}=="04A629B8AB87AB8111ECB2A38815028", ENV{ID_USB_INTERFACE_NUM}=="01", SYMLINK+="ttyRISCV1", MODE="0666", GROUP="dialout"
+
+    # Orin AGX1
+    SUBSYSTEM=="tty", KERNEL=="ttyACM[0-9]*", ATTRS{serial}=="TOPOC39C0EE1", ENV{ID_USB_INTERFACE_NUM}=="01", SYMLINK+="ttyAGX1", MODE="0666", GROUP="dialout"
+
+    # Orin AGX64
+    SUBSYSTEM=="tty", KERNEL=="ttyACM[0-9]*", ATTRS{serial}=="TOPOB63758F2", ENV{ID_USB_INTERFACE_NUM}=="01", SYMLINK+="ttyAGX64", MODE="0666", GROUP="dialout"
+  '';
+
+  # Trigger UDEV rules
+  system.activationScripts.udevTrigger = ''
+    echo "==> Triggering udev rules for already plugged devices..."
+    /run/current-system/sw/bin/udevadm trigger --subsystem-match=tty
   '';
 
   # Details of the hardware devices connected to this host
@@ -72,7 +83,7 @@
         relay_serial_port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A10KYI3B-if00-port0";
         OrinAGX1 = {
           inherit location;
-          serial_port = "/dev/ttyACM0";
+          serial_port = "/dev/ttyAGX1";
           relay_number = 4;
           device_ip_address = "172.18.16.54";
           socket_ip_address = "NONE";
@@ -94,17 +105,17 @@
           ext_drive_by-id = "usb-Samsung_PSSD_T7_S6XPNS0W606359P-0:0";
           threads = 8;
         };
-        NUC1 = {
+        OrinAGX64 = {
           inherit location;
           relay_number = 1;
-          serial_port = "/dev/ttyNUC1";
+          serial_port = "/dev/ttyAGX64";
           device_ip_address = "172.18.16.16";
           socket_ip_address = "NONE";
           plug_type = "NONE";
           switch_bot = "NONE";
           usbhub_serial = "0x029CEAF3";
           ext_drive_by-id = "usb-Samsung_PSSD_T7_S6XNNS0W201129V-0:0";
-          threads = 8;
+          threads = 12;
         };
         LenovoX1-1 = {
           inherit location;
