0bfc6cba205245ceed81ece503db035d63b0e074
Joonas Rautiola <joonas.rautiola@unikie.com>
2025-07-30 12:30:09 +0300

    Use nix 2.30 from unstable
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Allows us to structure the flake with the NixOS module system
     flake-parts.url = "github:hercules-ci/flake-parts";
