264142a04c1927e4939e5d9de820e8025d87e24f
Fayad Fami <fayad.fami@tii.ae>
2025-07-31 05:06:37 +0000

    Add newly provisioned volume for nix
    Signed-off-by: Fayad Fami <fayad.fami@tii.ae>
    
    Add volume for nix on ci-prod
    
    Signed-off-by: Fayad Fami <fayad.fami@tii.ae>

--- hosts/hetzci/prod/disk-config.nix
+++ hosts/hetzci/prod/disk-config.nix
@@ -1,31 +1,53 @@
 # SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  disko.devices.disk.os = {
-    device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_60668209";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        boot = {
-          type = "EF02";
-          size = "1M";
-        };
-        ESP = {
-          type = "EF00";
-          size = "256M";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
+  disko.devices = {
+    disk = {
+      disk0 = {
+        device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_60668209";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              type = "EF02";
+              size = "1M";
+            };
+            ESP = {
+              type = "EF00";
+              size = "256M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
           };
         };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+      };
+
+      disk1 = {
+        device = "/dev/disk/by-id/scsi-0HC_Volume_102993813";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            nix = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/nix";
+              };
+            };
           };
         };
       };
