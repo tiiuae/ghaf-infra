fdd4be35dcb1143243e5e95fe21085f775c96c1f
Henri Rosten <henri.rosten@unikie.com>
2025-09-03 14:00:31 +0300

    Start using hetz86-rel-1 builder in hetzci-release
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- hosts/hetzci/release/configuration.nix
+++ hosts/hetzci/release/configuration.nix
@@ -1,19 +1,29 @@
 # SPDX-FileCopyrightText: 2022-2025 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{ lib, ... }:
+{
+  lib,
+  config,
+  machines,
+  ...
+}:
 {
   imports = [
     ./disk-config.nix
     ../common.nix
     ../jenkins.nix
-    ../remote-builders.nix
     ../cloud.nix
     ../auth.nix
   ];
 
   system.stateVersion = lib.mkForce "25.05";
   networking.hostName = "hetzci-release";
-  sops.defaultSopsFile = ./secrets.yaml;
+
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    secrets = {
+      ssh_private_key.owner = "root";
+    };
+  };
 
   hetzci = {
     jenkins = {
@@ -36,5 +46,48 @@
       "x-systemd.makefs"
       "x-systemd.growfs"
     ];
+  };
+
+  # Configure (release) remote builders
+  nix = {
+    distributedBuilds = true;
+    buildMachines =
+      let
+        commonOptions = {
+          maxJobs = 16;
+          speedFactor = 10;
+          supportedFeatures = [
+            "kvm"
+            "nixos-test"
+            "benchmark"
+            "big-parallel"
+          ];
+          sshUser = "hetzci-release";
+          sshKey = config.sops.secrets.ssh_private_key.path;
+        };
+      in
+      [
+        (
+          {
+            hostName = "hetz86-rel-1";
+            system = "x86_64-linux";
+          }
+          // commonOptions
+        )
+      ];
+  };
+
+  programs.ssh = {
+    # Known builder host public keys, these go to /root/.ssh/known_hosts
+    knownHosts = {
+      "hetz86-rel-1".publicKey = machines.hetz86-rel-1.publicKey;
+      "${machines.hetz86-rel-1.ip}".publicKey = machines.hetz86-rel-1.publicKey;
+    };
+
+    # Custom options to /etc/ssh/ssh_config
+    extraConfig = lib.mkAfter ''
+      Host hetz86-rel-1
+      Hostname ${machines.hetz86-rel-1.ip}
+    '';
   };
 }
