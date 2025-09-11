cf92c805c5556729eab016c36b758c95fd7584c7
Henri Rosten <henri.rosten@unikie.com>
2025-09-11 08:58:40 +0300

    Add cachix release cache
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- hosts/builders/hetz86-rel-1/configuration.nix
+++ hosts/builders/hetz86-rel-1/configuration.nix
@@ -4,7 +4,6 @@
   lib,
   self,
   inputs,
-  config,
   ...
 }:
 {
@@ -26,7 +25,6 @@
   sops = {
     defaultSopsFile = ./secrets.yaml;
     secrets = {
-      cachix-auth-token.owner = "root";
       loki_password.owner = "promtail";
     };
   };
@@ -55,20 +53,15 @@
     "vm.page-cluster" = 0;
   };
 
-  # Disable cachix push for now, until we setup an own
-  # cache for release builds
-  services.cachix-watch-store = {
-    enable = false;
-    verbose = true;
-    cacheName = "ghaf-dev";
-    cachixTokenFile = config.sops.secrets.cachix-auth-token.path;
-  };
-
-  # Ensure only the nixos.org cache is trusted
+  # Ensure only the nixos.org and ghaf-release cachix cache are trusted
   nix.settings.trusted-public-keys = lib.mkForce [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "ghaf-release.cachix.org-1:wvnAftt8aSJ5KukTQb+BvvZYqJ5qzWEk/QHMbn2o+Ag="
   ];
-  nix.settings.substituters = lib.mkForce [ "https://cache.nixos.org/" ];
+  nix.settings.substituters = lib.mkForce [
+    "https://cache.nixos.org/"
+    "https://ghaf-release.cachix.org"
+  ];
   nix.settings.extra-trusted-public-keys = lib.mkForce [ "" ];
   nix.settings.extra-substituters = lib.mkForce [ "" ];
   nix.settings.trusted-substituters = lib.mkForce [ "" ];
--- hosts/hetzci/release/configuration.nix
+++ hosts/hetzci/release/configuration.nix
@@ -48,11 +48,15 @@
     ];
   };
 
-  # Ensure only the nixos.org cache is trusted
+  # Ensure only the nixos.org and ghaf-release cachix cache are trusted
   nix.settings.trusted-public-keys = lib.mkForce [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "ghaf-release.cachix.org-1:wvnAftt8aSJ5KukTQb+BvvZYqJ5qzWEk/QHMbn2o+Ag="
   ];
-  nix.settings.substituters = lib.mkForce [ "https://cache.nixos.org/" ];
+  nix.settings.substituters = lib.mkForce [
+    "https://cache.nixos.org/"
+    "https://ghaf-release.cachix.org"
+  ];
   nix.settings.extra-trusted-public-keys = lib.mkForce [ "" ];
   nix.settings.extra-substituters = lib.mkForce [ "" ];
   nix.settings.trusted-substituters = lib.mkForce [ "" ];
