bec021b5eb04331330305d3d946bc12c0e5ab204
Joonas Rautiola <joonas.rautiola@unikie.com>
2025-09-19 13:46:55 +0300

    Add copyright headers
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- pkgs/nethsm-exporter/default.nix
+++ pkgs/nethsm-exporter/default.nix
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
 { python3Packages, ... }:
 python3Packages.buildPythonApplication rec {
   pname = "nethsm-exporter";
