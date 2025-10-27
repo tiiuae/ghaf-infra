fd573acc5bd424141123d27c7558ecc84e1e1cc6
Joonas Rautiola <joonas.rautiola@unikie.com>
2025-10-27 12:10:25 +0200

    Expose new packages
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- nix/packages.nix
+++ nix/packages.nix
@@ -9,6 +9,8 @@
         nethsm-exporter = pkgs.callPackage ../pkgs/nethsm-exporter { };
         pkcs11-proxy = pkgs.callPackage ../pkgs/pkcs11-proxy { };
         systemd-sbsign = pkgs.callPackage ../pkgs/systemd-sbsign { };
+        nethsm-pkcs11 = pkgs.callPackage ../pkgs/nethsm-pkcs11 { };
+        softhsm2 = pkgs.callPackage ../pkgs/softhsm2 { };
 
         # Vendored in, as brainstem isn't suitable for nixpkgs packaging upstream:
         # https://github.com/NixOS/nixpkgs/pull/313643
