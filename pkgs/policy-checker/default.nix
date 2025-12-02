ba6f7ea99fccbbc4cf27d60823f8a69578920688
Joonas Rautiola <joonas.rautiola@unikie.com>
2025-12-02 17:18:44 +0200

    Verify provenance signature using x509 certificate
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- pkgs/policy-checker/default.nix
+++ pkgs/policy-checker/default.nix
@@ -12,7 +12,7 @@ buildGoModule {
   version = "0.1";
 
   src = lib.cleanSource ./src;
-  vendorHash = "sha256-1/L9BKQeHCP5PUogvxMFKaOwK5s/EKqcn/1eDWYtPOo=";
+  vendorHash = "sha256-0P/vmGoSYBSj4lLyw56vn8jEOP9innq53QXpLrXUKf0=";
   nativeBuildInputs = [ makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/policy-checker --prefix PATH : "${openssl}/bin"
