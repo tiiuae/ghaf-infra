7856334625cc986674087839657d501764433457
Kai Tusa <kai.tusa@unikie.com>
2025-12-02 16:39:20 +0200

    Rotate ktu key
    Signed-off-by: Kai Tusa <kai.tusa@unikie.com>

--- users/ktu.nix
+++ users/ktu.nix
@@ -6,7 +6,7 @@
       description = "Kai Tusa";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBeMFr++WulL/hQKejDnE1ePRQscLp7LvLAy/DyLW4AU ktu@nixos"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMW15uyZmW0ICy2YxwiWAWqLP4NmOI8YsswXgdavwqVS x1"
       ];
       extraGroups = [
         "wheel"
