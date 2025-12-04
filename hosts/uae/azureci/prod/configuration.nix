74f95e708b2a62dc87ba70a3df9a66eea1842997
fayad <fayad.fami@tii.ae>
2025-12-04 16:24:59 +0400

    Deploy prod hetzci on Azure UAE
    Import jenkins and hetzci modules.
    Update host key for uae-azureci-prod.
    Redeploy host configuration with services.
    Signing not enabled atm.
    Move common configs to azure-common.nix.
    Reuses all hetzci configurations as modules.
    
    Signed-off-by: fayad <fayad.fami@tii.ae>

--- hosts/uae/azureci/prod/configuration.nix
+++ hosts/uae/azureci/prod/configuration.nix
@@ -1,8 +1,6 @@
 # SPDX-FileCopyrightText: 2022-2025 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  self,
-  pkgs,
   inputs,
   modulesPath,
   lib,
@@ -12,45 +10,32 @@
   imports = [
     ./disk-config.nix
     ../azure-common.nix
+    ../remote-builders.nix
+    ../../../hetzci/auth.nix
+    ../../../hetzci/common.nix
+    ../../../hetzci/jenkins.nix
     (modulesPath + "/profiles/qemu-guest.nix")
     inputs.disko.nixosModules.disko
-  ]
-  ++ (with self.nixosModules; [
-    common
-    service-openssh
-    team-devenv
-    user-bmg
-  ]);
+  ];
 
   # this server has been initialized with 25.05 with nixos-anywhere
   # initializing fails with 24.11
   system.stateVersion = lib.mkForce "25.05";
+  sops.defaultSopsFile = ./secrets.yaml;
 
-  nixpkgs.hostPlatform = "x86_64-linux";
-  hardware.enableRedistributableFirmware = true;
+  networking.hostName = "uae-azureci-prod";
 
-  networking = {
-    hostName = "uae-azureci-prod";
-  };
-
-  boot = {
-    # use predictable network interface names (eth0)
-    kernelParams = [ "net.ifnames=0" ];
-    loader.grub = {
-      efiSupport = true;
-      efiInstallAsRemovable = true;
+  hetzci = {
+    jenkins = {
+      envType = "prod";
+      casc = ./casc;
+      pluginsFile = ./plugins.json;
+      withCachix = false;
+    };
+    auth = {
+      clientID = "azureci-prod";
+      domain = "ci-prod.uaenorth.cloudapp.azure.com";
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    screen
-    tmux
-    cryptsetup
-    sg3_utils
-    dnsutils
-    inetutils
-    pciutils
-    dmidecode
-    jq
-  ];
 }
