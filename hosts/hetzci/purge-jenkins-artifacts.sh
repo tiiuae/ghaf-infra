0ebd0b00acdfca0150a68ded146bb3eb2b16ca10
Henri Rosten <henri.rosten@unikie.com>
2026-01-13 13:44:45 +0200

    Restructure purge-jenkins-artifacts.sh
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- hosts/hetzci/purge-jenkins-artifacts.sh
+++ hosts/hetzci/purge-jenkins-artifacts.sh
@@ -8,24 +8,40 @@ set -E # exit immediately if a command fails (subshells)
 set -u # treat unset variables as an error and exit
 
 # Expected arguments and their defaults if not passed in environment variables
-# Always keep at least this many build artifacts for each pipeline
-PURGE_KEEP_BUILDS="${PURGE_KEEP_BUILDS:=3}"
-# Skip purge if nix store disk usage is less than PURGE_DU_PCT percent
-PURGE_DU_PCT="${PURGE_DU_PCT:=90}"
+# Trigger purge when disk usage is more than PURGE_DU_PCT percent
+PURGE_DU_PCT="${PURGE_DU_PCT:=85}"
 
-nix_store_du_pct=$( (df /nix/store --output=pcent || df /nix --output=pcent || df / --output=pcent) | tr -dc '0-9')
-echo "nix store disk usage: $nix_store_du_pct%"
-if ((nix_store_du_pct < PURGE_DU_PCT)); then
-  exit 0
+purge() {
+  # Cleanup /tmp: delete large files owned by jenkins modified at least 24 hours ago
+  echo "Cleanup files from /tmp"
+  find /tmp/ -mindepth 1 -type f -size +1G -user jenkins -mtime +1 -print -delete 2>/dev/null || true
+  # Remove jenkins artifacts from /var/lib/jenkins/artifacts.
+  # Outer loop finds the directories directly under /var/lib/jenkins/artifacts/,
+  # that is, the per-pipeline artifacts directories. Inner loop removes the oldest
+  # build on each pipeline, always keeping at least the current newest build.
+  echo "Cleanup jenkins artifacts"
+  while IFS= read -r path; do
+    find "$path" -maxdepth 0 -exec ls -rt {} + | head -n -1 | head -n +1 | while read -r x; do
+      echo "Removing '$path/$x'"
+      rm -fr "${path:?}/${x:?}"
+    done
+  done < <(find /var/lib/jenkins/artifacts -maxdepth 1 -mindepth 1 -type d)
+}
+
+opt='--output=pcent'
+du_jenkins_pct=$( (df /var/lib/jenkins/artifacts $opt || df / $opt) | tr -dc '0-9')
+du_nix_store_pct=$( (df /nix/store $opt || df /nix $opt || df / $opt) | tr -dc '0-9')
+echo "Disk usage on jenkins artifacts disk: $du_jenkins_pct%"
+echo "Disk usage on nix store disk: $du_nix_store_pct%"
+if ((du_jenkins_pct > PURGE_DU_PCT)) || ((du_nix_store_pct > PURGE_DU_PCT)); then
+  purge
 fi
-
-echo "Purge artifacts; removing all but the last $PURGE_KEEP_BUILDS builds on each pipeline"
-# Outer loop finds the directories directly under /var/lib/jenkins/artifacts/,
-# that is, the per-pipeline artifacts directories.
-# Inner loop removes all but the latest PURGE_KEEP_BUILDS builds on each pipeline
-while IFS= read -r path; do
-  find "$path" -maxdepth 0 -exec ls -rt {} + | head -n -"$PURGE_KEEP_BUILDS" | while read -r x; do
-    echo "Removing '$path/$x'"
-    rm -fr "${path:?}/${x:?}"
-  done
-done < <(find /var/lib/jenkins/artifacts -maxdepth 1 -mindepth 1 -type d)
+if ((du_nix_store_pct > PURGE_DU_PCT)); then
+  # Trigger nix gc if the cleanup was initiated due to nix store disk usage. Respect
+  # the max-free config: collect garbage until at least max-free bytes have been
+  # deleted, then stop.
+  maxfree=$(nix config show | grep 'max-free =' | tr -dc '0-9')
+  maxfree="${maxfree:=100G}" # default, if parsing nix config show failed
+  echo "Trigger nix garbage collection (max-free=$maxfree)"
+  nix-store --gc --max-freed "$maxfree"
+fi
