a80ce9d73bebdc2a024ce07d59590bfa53079c0d
Kai Tusa <kai.tusa@unikie.com>
2026-01-30 10:02:26 +0200

    Add Darter Pro storeOnDisk debug target
    Signed-off-by: Kai Tusa <kai.tusa@unikie.com>

--- hosts/hetzci/pipelines/ghaf-nightly.groovy
+++ hosts/hetzci/pipelines/ghaf-nightly.groovy
@@ -65,6 +65,9 @@ def TARGETS = [
   [ target: "packages.x86_64-linux.system76-darp11-b-storeDisk-debug-installer",
     testset: null, uefisigniso: true, sbom: true,
   ],
+  [ target: "packages.x86_64-linux.system76-darp11-b-storeDisk-debug",
+    testset: null, sbom: true,
+  ],
 ]
 
 pipeline {
