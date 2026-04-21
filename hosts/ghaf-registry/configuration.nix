1bf29865c3868ea5da6ab1dba9731cdb2b3a117f
Joonas Rautiola <joonas.rautiola@unikie.com>
2026-04-21 19:13:42 +0300

    Use S3 backend and configure retention for zot registry
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- hosts/ghaf-registry/configuration.nix
+++ hosts/ghaf-registry/configuration.nix
@@ -10,13 +10,62 @@
 }:
 let
   inherit (self.packages.${pkgs.stdenv.hostPlatform.system}) zot;
+
   zotDataDir = "/var/lib/zot";
   zotPort = 5000;
+
   zotConfig = {
     storage = {
       rootDirectory = zotDataDir;
+
+      storageDriver = {
+        name = "s3";
+        bucket = "oci-artifacts";
+        region = "hel1";
+        forcepathstyle = true;
+        regionendpoint = "https://hel1.your-objectstorage.com";
+        chunksize = toString (32 * 1024 * 1024);
+      };
+
+      dedupe = false;
       gc = true;
-      gcDelay = "1h";
+      gcInterval = "1h";
+      retention = {
+        delay = "24h";
+        policies = [
+          {
+            repositories = [ "ghaf/**" ];
+            deleteReferrers = true;
+            deleteUntagged = true;
+            keepTags = [
+              {
+                patterns = [ "release-.*" ];
+                # Keep at least three builds in each repository
+                mostRecentlyPushedCount = 3;
+                # Keep everything for at least a week
+                pushedWithin = "168h";
+              }
+              {
+                patterns = [ "prod-.*" ];
+                mostRecentlyPushedCount = 3;
+                pushedWithin = "168h";
+              }
+              {
+                patterns = [ "dev-.*" ];
+                mostRecentlyPushedCount = 3;
+                pushedWithin = "168h";
+              }
+              {
+                patterns = [
+                  "vm-.*"
+                  "dbg-.*"
+                ];
+                pushedWithin = "24h";
+              }
+            ];
+          }
+        ];
+      };
     };
 
     http = {
@@ -24,19 +73,23 @@ let
       port = zotPort;
       externalUrl = "https://registry.vedenemo.dev";
 
-      auth.htpasswd.path = config.sops.secrets.zot-htpasswd.path;
-      auth.openid.providers.oidc = {
-        name = "Vedenemo Auth";
-        issuer = "https://auth.vedenemo.dev";
-        credentialsFile = config.sops.templates."zot-oidc-credentials.json".path;
-        keypath = "";
-        scopes = [
-          "openid"
-          "profile"
-          "email"
-          "groups"
-        ];
+      auth = {
+        htpasswd.path = config.sops.secrets.zot-htpasswd.path;
+
+        openid.providers.oidc = {
+          name = "Vedenemo Auth";
+          issuer = "https://auth.vedenemo.dev";
+          credentialsFile = config.sops.templates."zot-oidc-credentials.json".path;
+          keypath = "";
+          scopes = [
+            "openid"
+            "profile"
+            "email"
+            "groups"
+          ];
+        };
       };
+
       accessControl = {
         repositories = {
           "ghaf/**" = {
@@ -92,8 +145,11 @@ in
   networking.hostName = "ghaf-registry";
 
   sops = {
-    secrets.auth-client-secret.owner = "zot";
-    secrets.zot-htpasswd.owner = "zot";
+    secrets = {
+      auth-client-secret.owner = "zot";
+      zot-s3-credentials.owner = "zot";
+      zot-htpasswd.owner = "zot";
+    };
     templates."zot-oidc-credentials.json" = {
       owner = "zot";
       content = builtins.toJSON {
@@ -110,8 +166,12 @@ in
 
     locations."/" = {
       proxyPass = "http://127.0.0.1:${toString zotPort}";
+      # nginx buffering causes pulls to fail occasionally when using s3 backend
       extraConfig = ''
         client_max_body_size 0;
+        proxy_buffering off;
+        proxy_request_buffering off;
+        proxy_max_temp_file_size 0;
         proxy_read_timeout 3600s;
         proxy_send_timeout 3600s;
       '';
@@ -149,6 +209,7 @@ in
       RestartSec = "5s";
 
       UMask = "0027";
+      EnvironmentFile = config.sops.secrets.zot-s3-credentials.path;
       WorkingDirectory = zotDataDir;
       ReadWritePaths = [ zotDataDir ];
     };
