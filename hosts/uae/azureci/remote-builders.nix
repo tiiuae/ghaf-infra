4f9d2bdf3aff7f3c56a190e0e00861d09d26d7ea
fayad <fayad.fami@tii.ae>
2026-04-28 09:22:48 +0400

    Use intra VPC connection for UAE azure
    ` Switch to builder private IP for azure intra vpc connection
    ` Do not route through internet for azure builders
    
    Signed-off-by: fayad <fayad.fami@tii.ae>

--- hosts/uae/azureci/remote-builders.nix
+++ hosts/uae/azureci/remote-builders.nix
@@ -45,7 +45,7 @@ in
       [
         (
           {
-            hostName = "az86-1.uaenorth.cloudapp.azure.com";
+            hostName = "10.51.16.5";
             system = "x86_64-linux";
             maxJobs = x86BuilderMaxJobs;
           }
@@ -65,14 +65,14 @@ in
   programs.ssh = {
     # Known builder host public keys, these go to /root/.ssh/known_hosts
     knownHosts = {
-      "az86-1.uaenorth.cloudapp.azure.com".publicKey = machines.uae-azureci-az86-1.publicKey;
+      "10.51.16.5".publicKey = machines.uae-azureci-az86-1.publicKey;
       "91.98.90.243".publicKey = machines.uae-azureci-hetzarm-1.publicKey;
     };
 
     # Custom options to /etc/ssh/ssh_config
     extraConfig = lib.mkAfter ''
-      Host az86-1.uaenorth.cloudapp.azure.com
-      Hostname az86-1.uaenorth.cloudapp.azure.com
+      Host 10.51.16.5
+      Hostname 10.51.16.5
       User uae-remote-build
       IdentityFile ${config.sops.secrets.azureci_builder_ssh_key.path}
 
