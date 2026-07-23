9014f74e6aa8544f50366671de090ab9c3d7ef50
Joonas Rautiola <joonas.rautiola@unikie.com>
2026-07-23 12:09:42 +0300

    pkgs/nethsm-pkcs11: 2.0.0 -> 2.2.0
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- pkgs/nethsm-pkcs11/default.nix
+++ pkgs/nethsm-pkcs11/default.nix
@@ -7,16 +7,15 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "nethsm-pkcs11";
-  version = "2.0.0";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     owner = "Nitrokey";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-qTuRBwXrl+tqswmwVHBblVL9JJGMImh4L+jyByViKaQ=";
+    hash = "sha256-SrranXfMUOckgRVGQTiD9os5H37eTc6G8Ayfbm5Fq38=";
   };
 
-  cargoHash = "sha256-j1M9TUXdK1+8Zl9XV4uxQDbR9RUwoAnB0OctrUksvKs=";
-
-  # meta.mainProgram = "pkcs11-daemon";
+  dontUseCargoParallelTests = true;
+  cargoHash = "sha256-2OcnOrTGk8pv32Iv1tQKq8CT9BhkDDLHxfdzQRJnTsQ=";
 }
