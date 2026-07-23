a1c0c8606c8abd5a9ba47ec18d74c3e10d071f96
Joonas Rautiola <joonas.rautiola@unikie.com>
2026-07-23 12:10:37 +0300

    pkgs/softhsm2: 25b94d -> 2.7.0
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- pkgs/softhsm2/default.nix
+++ pkgs/softhsm2/default.nix
@@ -9,20 +9,15 @@
   sqlite,
   ...
 }:
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "softhsm2";
-
-  # nixpkgs provides 2.6.1, which is from 2020.
-  # softhsm2 has not had a new release or tag in over 5 years.
-  # Using the latest from git is better than using an outdated release.
-  version = "develop";
+  version = "2.7.0";
 
   src = fetchFromGitHub {
     owner = "softhsm";
     repo = "SoftHSMv2";
-    # head of develop branch
-    rev = "25b94d4752739fc5954e7eb3a404810db5d632fa";
-    sha256 = "sha256-yjcS8Jm7XAEqa1DrE0FcbccvubcRl+UlUeNp56NUVi8=";
+    tag = version;
+    sha256 = "sha256-gwqdgGCVPQwPkE6gFlZxZdk6Ln/qZn3CmMfbcLm9p04=";
   };
 
   nativeBuildInputs = [
