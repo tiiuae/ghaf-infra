53c6794ffd9423a6d52f65d354e48fc94b688b7b
Fayad Fami <93858662+fayadf@users.noreply.github.com>
2026-07-27 10:48:12 +0400

    Scale up UAE registry for hetzci
    Redeploy UAE registry with big disk to handle hetzci workload.
    Initialized with 26.05.
    
    Signed-off-by: fayad <fayad.fami@tii.ae>

--- hosts/uae/azureci/registry/disk-config.nix
+++ hosts/uae/azureci/registry/disk-config.nix
@@ -1,33 +1,44 @@
 # SPDX-FileCopyrightText: 2022-2025 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  disko.devices.disk.os = {
-    device = "/dev/disk/by-id/scsi-3600224805796e441822ca750e27c9309";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        boot = {
-          type = "EF02";
-          size = "1M";
-        };
-        ESP = {
-          type = "EF00";
-          size = "1024M";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
+  disko.devices.disk = {
+    os = {
+      device = "/dev/disk/by-id/scsi-3600224802f7903d421d1e5228d90c98f";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            type = "EF02";
+            size = "1M";
+          };
+          ESP = {
+            type = "EF00";
+            size = "1024M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
           };
         };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        };
+      };
+    };
+    data-disk1 = {
+      device = "/dev/disk/by-id/scsi-3600224804d4dbfa9e813969dda6e0606";
+      type = "disk";
+      content = {
+        type = "filesystem";
+        format = "xfs";
+        mountpoint = "/var/lib/zot";
       };
     };
   };
