1b03a3454e4de11e2abfa8f44071332b9e178b66
Henri Rosten <henri.rosten@unikie.com>
2026-09-14 14:09:10 +0300

    Revert "Unify kernel versions"
    This reverts commit df695e8a214c40f2079f231720b9bebe4c8cb8aa.
    
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- tasks.py
+++ tasks.py
@@ -85,10 +85,6 @@ KEXEC_IMAGES = {
         f"{NIXOS_IMAGES_URL}/nixos-26.05/"
         "nixos-kexec-installer-noninteractive-aarch64-linux.tar.gz"
     ),
-    "hetzarm-dbg-1": (
-        f"{NIXOS_IMAGES_URL}/nixos-26.05/"
-        "nixos-kexec-installer-noninteractive-aarch64-linux.tar.gz"
-    ),
 }
 
 RELEASE_BUILDER_USERS = (
