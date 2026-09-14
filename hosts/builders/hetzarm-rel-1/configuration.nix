37ae0d58b207a70d442d6afe766f7c843ef78878
Henri Rosten <henri.rosten@unikie.com>
2026-09-14 14:09:10 +0300

    Revert "Unify kernel versions"
    This reverts commit df695e8a214c40f2079f231720b9bebe4c8cb8aa.
    
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- hosts/builders/hetzarm-rel-1/configuration.nix
+++ hosts/builders/hetzarm-rel-1/configuration.nix
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2022-2025 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
+  pkgs,
   self,
   inputs,
   ...
@@ -57,4 +58,6 @@
   nix.settings.trusted-users = [
     "hetzarm-rel-1-builder"
   ];
+  # Nixos-anywhere kexec switch fails on hetzner cloud arm VMs without this
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
